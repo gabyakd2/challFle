fa7c5cb68bf8c4e7bb1fa0b6ad83102dfdbe77eb
gabyakd2 <gabrielsal00@hotmail.com>
2023-03-03 18:14:03 -0300

    cambio boton para limpiar

--- react-search-bar/src/components/SearchBar/SearchBar.jsx
+++ react-search-bar/src/components/SearchBar/SearchBar.jsx
@@ -51,7 +51,6 @@ export default function SearchBar({ productsList, setProductList, setIsLoading }
               xmlns="http://www.w3.org/2000/svg"
               width="16"
               fill="currentColor"
-              // className="mb-1"
               viewBox="0 0 16 16"
             >
               <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
@@ -61,14 +60,13 @@ export default function SearchBar({ productsList, setProductList, setIsLoading }
         </InputGroup>
       </Form>
       {input.length ? (
-        <Button variant="dark" className="mt-2">
+        <Button variant="dark" className="mt-2" onClick={handleCloseInput}>
           {input}{" "}
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="13"
             fill="currentColor"
             viewBox="0 0 16 16"
-            onClick={handleCloseInput}
           >
             <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z" />
           </svg>
